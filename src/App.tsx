08f01de874186a7b4e08c3d3d0e2615722cdfd05
German Zarkovich <68071939+gerzarko@users.noreply.github.com>
2023-07-05 23:35:15 -0400

    Update with functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import type { Component } from "solid-js";
-import { presidents } from "us-presidents";
+import { createSignal, type Component, type JSX } from "solid-js";
+import { President, presidents } from "us-presidents";
+
+interface presidents {
+  name: string;
+  term: {
+    startYear: number;
+    endYear: number;
+  };
+}
+const [president, setPresident] = createSignal<President>(randomPresident());
+
+function randomPresident(): President {
+  return presidents[Math.floor(Math.random() * presidents.length)];
+}
+
+function changePresident(): void {
+  setPresident(randomPresident());
+}
 
 const App: Component = () => {
-  const presidentsList = presidents.map((president) => {
-    return (
-      <div class="flex flex-col">
-        <div>
-          <h2>
-            {president.name}: {president.term.startYear} to{" "}
-            {president.term.endYear}
-          </h2>
-        </div>
-      </div>
-    );
-  });
-
   return (
-    <div class="w-full">
-      <h1 class="text-md text-center">{presidentsList}</h1>
+    <div class="flex flex-col">
+      <div>
+        <h2>
+          {president().name} {president().term.startYear} -{" "}
+          {president().term.endYear}
+        </h2>
+        <button onclick={changePresident}>random</button>
+      </div>
     </div>
   );
 };
